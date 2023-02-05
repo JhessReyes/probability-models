80d028df1146847f1c2dbeea7630cae90f2c7b45
JhessReyes <jhessaureyes@gmail.com>
2023-02-05 01:05:00 -0600

    daisy ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="mr-10 ml-10" id="home">
+  <div class="sm:columns-1 columns-1 w-full">
+    <div class="overflow-auto rounded-xl shadow-xl">
+      <h1 class="text-lg text-center text-primary font-extrabold">DASHBOARD</h1>
+    </div>
+  </div>
+</section>
